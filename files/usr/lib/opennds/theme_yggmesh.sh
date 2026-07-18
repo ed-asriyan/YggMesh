17583693a96b93ae377a07f9a799dec7e30c6c31
Ed Asriyan <github@asriyan.me>
2026-07-18 20:44:35 +0000

    Update splash page and theme script for improved functionality and clarity

--- files/usr/lib/opennds/theme_yggmesh.sh
+++ files/usr/lib/opennds/theme_yggmesh.sh
@@ -1,8 +1,20 @@
 #!/bin/sh
 
+# Override the default header/footer so libopennds.sh does not
+# prepend its own <html> document before ours (our splash.html
+# is a full standalone document).
+header() {
+    :
+}
+
+footer() {
+    exit 0
+}
+
 generate_splash_sequence() {
-    authtarget="/opennds_auth/"
-    sed -e "s|\$authtarget|$authtarget|g" -e "s|\$tok|$tok|g" /etc/opennds/htdocs/splash.html
+    authtarget="/opennds_preauth/"
+    sed -e "s|\$authtarget|$authtarget|g" -e "s|\$fas|$fas|g" /etc/opennds/htdocs/splash.html
+    footer
 }
 
 . /usr/lib/opennds/libopennds.sh
